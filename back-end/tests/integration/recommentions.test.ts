7c0ae8810d9995236a10416f7d1e0208d399f60d
Audrey Costa <audreycota@nano.ufrj.br>
2022-09-26 15:35:49 -0300

    test(integrationTest): Test the route GET /recommendations/:id to get a recommendation by id This route return the recommendation passed by id by params and return status code 200. If no one recommendation with the designed id is found return status code 404.

--- back-end/tests/integration/recommentions.test.ts
+++ back-end/tests/integration/recommentions.test.ts
@@ -100,14 +100,61 @@ describe("Test the route /recommendations/top/:amount", ()=>{
         
         for(let i = 0; i < recommendations.length; i++){
             await prisma.recommendation.create({data: recommendations[i]});
-        }
-        
+        };
+
         const result = await supertest(app).get(`/recommendations/top/${amount}`);
         
         expect(result.status).toBe(200);
         expect(result.body.length).toEqual(amount);
     });
 });
+
+describe("Test the route /recommendatons/:id", ()=>{
+    it("Return status 200 and response with the respective recommendation given id by params", async ()=>{
+        const recommendations = [{
+            name: "Eminem - Stan (Long Version) ft. Dido",
+            youtubeLink: "https://www.youtube.com/watch?v=gOMhN-hfMtY",
+        }, {
+            name: "ilomilo (Audio)",
+            youtubeLink: "https://www.youtube.com/watch?v=-e7wiyNO2us&list=RDEMcce0hP5SVByOVCd8UWUHEA&index=3",
+        }, {
+            name: "Billie Eilish - NDA", 
+            youtubeLink: "https://www.youtube.com/watch?v=Dm9Zf1WQ_A&list=RDEMcce0hP5SVByOVCd8UWUHEA&index=3",
+        }, { 
+            name: "Billie Eilish - Bury a Friend", 
+            youtubeLink: "https://www.youtube.com/watch?v=DmZf1WYQ_A&list=RDEMcce0hP5SVByOVCd8UWUHEA&index=3",
+        } ,{
+            name: "Billie Eilish - Your Power", 
+            youtubeLink: "https://www.youtube.com/watch?v=Dm9ZfWYQ_A&list=RDEMcce0hP5SVByOVCd8UWUHEA&index=3",
+        }, {
+            name: "Billie Eilish - Everything I Wanted", 
+            youtubeLink: "https://www.youtube.com/watch?v=Dm9Zf1WYQ_A&list=RDEMcce0hP5SVByOVd8UWUHEA&index=3",
+        }, {
+            name: "Billie Eilish - Oceans Eyes", 
+            youtubeLink: "https://www.youtube.com/watch?v=Dm9Zf1WYQ_A&list=RDEMcce0h5SVByOVCd8UWUHEA&index=3",
+        }];
+
+        for(let i = 0; i < recommendations.length; i++){
+            await prisma.recommendation.create({data: recommendations[i]});
+        };
+        const recommendation = await prisma.recommendation.findFirst({where: {name: "Billie Eilish - Everything I Wanted"}});
+
+        const result = await supertest(app).get(`/recommendations/${recommendation.id}`);
+
+        expect(result.status).toBe(200);
+        expect(result.body).toStrictEqual(recommendation);
+    });
+
+    it("Return status 404", async()=>{
+        const id = 3;
+        
+        const result = await supertest(app).get(`/recommendations/${id}`);
+
+        expect(result.status).toBe(404);
+    })
+});
+
+
 
 afterAll(async () => {
     await prisma.$disconnect();
